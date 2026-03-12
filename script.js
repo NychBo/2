6423b7d2198f00a8a8183be7d18f5c20ddba0d97
NychBo <dark2dreamer2@gmail.com>
2026-03-12 16:37:36 +0200

    Update script.js

--- script.js
+++ script.js
@@ -7,66 +7,8 @@ el.innerText=el.dataset[lang]
 }
 
 
+window.onload=function(){
 
-let images=[]
-let index=0
-
-function openImage(img){
-
-images=[...document.querySelectorAll(".gallery img")]
-
-index=images.indexOf(img)
-
-document.getElementById("viewer").style.display="flex"
-document.getElementById("viewer-img").src=img.src
+setLang("ua")
 
 }
-
-function closeImage(){
-
-document.getElementById("viewer").style.display="none"
-
-}
-
-document.addEventListener("keydown",function(e){
-
-if(e.key==="ArrowRight"){
-
-index=(index+1)%images.length
-document.getElementById("viewer-img").src=images[index].src
-
-}
-
-if(e.key==="ArrowLeft"){
-
-index=(index-1+images.length)%images.length
-document.getElementById("viewer-img").src=images[index].src
-
-}
-
-})
-let startX=0
-
-document.getElementById("viewer").addEventListener("touchstart",e=>{
-startX=e.touches[0].clientX
-})
-
-document.getElementById("viewer").addEventListener("touchend",e=>{
-
-let endX=e.changedTouches[0].clientX
-
-if(endX-startX>50){
-
-index=(index-1+images.length)%images.length
-viewerImg.src=images[index].src
-
-}
-
-if(startX-endX>50){
-
-index=(index+1)%images.length
-viewerImg.src=images[index].src
-
-}
-
-})
